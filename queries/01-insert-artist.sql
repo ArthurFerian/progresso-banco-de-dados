2e1369df0816a69d184880638bf15a40e866a585
Arthur Ferian <arthur.ferian@aluno.senai.br>
2025-09-15 09:55:01 -0300

    feat: adiciona os primeiros valores

--- queries/01-insert-artist.sql
+++ queries/01-insert-artist.sql
@@ -8,3 +8,7 @@ ano_inicio INT,
 musica_mais_ouvida VARCHAR(50),
 ativo BOOLEAN DEFAULT TRUE,
 );
+
+INSERT INT artistas (nome, tipo, membros, genero_musical, pais_origem, ano_inicio, musica_mais_ouvida, ativo)
+VALUES 
+('Detentos do Rap', 'Grupo', 5, 'RAP', 'Brasil', 1990, 'Baseado Em Fatos Reais', TRUE);
