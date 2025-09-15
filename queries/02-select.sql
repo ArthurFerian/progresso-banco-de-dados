bbd385c556043cb598779cd7ec6a7490abed1c9a
Arthur Ferian <arthur.ferian@aluno.senai.br>
2025-09-15 10:49:48 -0300

    feat: finaliza select

--- queries/02-select.sql
+++ queries/02-select.sql
@@ -1,3 +1,15 @@
 SELECT COUNT(*) AS total_artistas FROM artistas;
 
 SELECT * FROM artistas; 
+
+SELECT nome, musica_mais_ouvida FROM artistas;
+
+SELECT nome, musica_mais_ouvida FROM artistas
+WHERE pais_origem = 'Brasil';
+
+
+SELECT nome, musica_mais_ouvida FROM artistas
+WHERE pais_origem = 'EUA';
+
+SELECT nome, musica_mais_ouvida FROM artistas
+WHERE id = 1;
